1bef70f0458e6740205bc5db0c1dbb2cf065f938
fzk <458813868@qq.com>
2019-07-18 16:35:12 +0800

    CDN

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/table2echarts/'
-    : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/table2echarts/' : '/',
+  productionSourceMap: false,
+  css: {
+    // 是否使用css分离插件 ExtractTextPlugin
+    extract: true,
+    // 开启 CSS source maps?
+    sourceMap: false,
+    // css预设器配置项
+    loaderOptions: {},
+    // 启用 CSS modules for all css / pre-processor files.
+    modules: false
+  },
+  configureWebpack: {
+    externals:{
+      'echarts': 'echarts'
+
+    }
+  }
 }
